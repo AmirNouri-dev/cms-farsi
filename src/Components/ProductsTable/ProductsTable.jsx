aea4fc40169519b7c5578ad48a5a6d0c4fc5d92a
amir <a.nouri1997@gmail.com>
2025-04-18 02:39:17 +0330

    create products table to show products

--- src/Components/ProductsTable/ProductsTable.jsx
+++ src/Components/ProductsTable/ProductsTable.jsx
@@ -2,5 +2,31 @@ import React from "react";
 import "./ProductsTable.css";
 
 export default function ProductsTable() {
-  return <div></div>;
+  return (
+    <table className="products-table">
+      <tr className="products-table-heading-tr">
+        <th>عکس</th>
+        <th>اسم</th>
+        <th>قیمت</th>
+        <th>موجودی</th>
+      </tr>
+      <tr className="products-table-body-tr">
+        <td>
+          <img
+            className="products-table-item-img"
+            src="./../../public/images/jambon.webp"
+            alt=""
+          />
+        </td>
+        <td>ژامبون میکس</td>
+        <td>230.000 تومان</td>
+        <td>65 عدد</td>
+        <td>
+          <button className="products-table-btn">جزییات</button>
+          <button className="products-table-btn">حذف</button>
+          <button className="products-table-btn">ویرایش</button>
+        </td>
+      </tr>
+    </table>
+  );
 }
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Products.css";
 import ErrorBox from "../../Components/ErrorBox/ErrorBox";
 import AddNewProduct from "../../Components/AddNewProduct/AddNewProduct";
+import ProductsTable from "../../Components/ProductsTable/ProductsTable";
 
 export default function Products() {
   return (
     <div>
       <AddNewProduct />
       <ErrorBox msg="هیچ محصولی یافت نشد !" />
+      <ProductsTable />
     </div>
   );
 }
