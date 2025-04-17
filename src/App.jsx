56351477d1181fdef06237453f1a11f4d733d173
amir <a.nouri1997@gmail.com>
2025-04-17 12:25:14 +0330

    create Sidebar component & use it in project

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <>
-      <h1>سبزلرن هستم</h1>
+      <Sidebar />
     </>
   );
 }
