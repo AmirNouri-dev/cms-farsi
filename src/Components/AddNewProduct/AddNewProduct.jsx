b548dc831810387cd6aa2a3ecea4ba3e2fa66ad6
amir <a.nouri1997@gmail.com>
2025-04-17 17:10:31 +0330

    create AddNewProduct comp -> import and use it in Products page

---
+++ src/Components/AddNewProduct/AddNewProduct.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./AddNewProduct.css";
+export default function AddNewProduct() {
+  return <div>new</div>;
+}
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Products.css";
 import ErrorBox from "../../Components/ErrorBox/ErrorBox";
+import AddNewProduct from "../../Components/AddNewProduct/AddNewProduct";
 
 export default function Products() {
   return (
     <div>
       <ErrorBox msg="هیچ محصولی یافت نشد !" />
+      <AddNewProduct />
     </div>
   );
 }
