f42b88782b8ab9f2cd32a4c171c8062fafca5ce1
amir <a.nouri1997@gmail.com>
2025-04-17 13:48:29 +0330

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
+import Header from "./Components/Header/Header";
 
 function App() {
   return (
     <>
       <Sidebar />
+      <div className="main">
+        <Header />
+        {/*route */}
+      </div>
     </>
   );
 }
