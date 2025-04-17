0d6f69a8bc97b0e51ba1267c9898fe4e099979fd
amir <a.nouri1997@gmail.com>
2025-04-17 16:01:00 +0330

    import BrowserRouter in main.jsx -> import Routes & Route in App.jsx + All pages -> create each page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Header from "./Components/Header/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import Comments from "./Pages/Comments/Comments";
+import Users from "./Pages/Users/Users";
+import Orders from "./Pages/Orders/Orders";
+import Offs from "./Pages/Offs/Offs";
+import Statistics from "./Pages/Statistics/Statistics";
 
 function App() {
   return (
@@ -11,6 +19,15 @@ function App() {
       <Sidebar />
       <div className="main">
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/comments" element={<Comments />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/offs" element={<Offs />} />
+          <Route path="/statistics" element={<Statistics />} />
+        </Routes>
         {/*route */}
       </div>
     </>
