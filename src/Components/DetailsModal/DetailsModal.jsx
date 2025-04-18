b05523000713579dbbfcf541156383f5fd03b407
amir <a.nouri1997@gmail.com>
2025-04-18 17:02:28 +0330

    create details modal and cms.css for shared modal styles

---
+++ src/Components/DetailsModal/DetailsModal.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./DetailsModal.css";
+export default function DetailsModal() {
+  return <div></div>;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./custom.css";
+import "./cms.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
